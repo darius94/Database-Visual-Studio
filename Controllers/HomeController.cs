9ee89c53920510bb89c52d334db452df9b8c6ea8
Darius Little <little_Darius@columbusstate.edu>
2017-03-07 21:44:39 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3_Webstore_.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Books()
+        {
+            ViewBag.Message = "Books available for purchase.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            ViewBag.Message = "Books left in stock.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3_Webstore_.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Category")]
+        [Required(ErrorMessage = "Category name is required")]
+        [MaxLength(45, ErrorMessage = "The category name can be maximum 45 characters long")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
